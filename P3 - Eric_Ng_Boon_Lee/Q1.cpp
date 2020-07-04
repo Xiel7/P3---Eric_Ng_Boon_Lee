37600c21e712ab9bd22ed5a8af91e418f349f759
Xiel7 <ericng402@gmail.com>
2020-07-04 22:16:52 +0800

    Add project files.

---
+++ P3 - Eric_Ng_Boon_Lee/Q1.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int count[10] = { 0 };
+//	int num, n;
+//
+//	for (int i = 0; i < 100; i++)
+//	{
+//		num = rand() % 10;
+//		cout << num << endl;
+//		count[num]++;
+//	}
+//
+//	n = 1;
+//	for (int x : count)
+//	{
+//		cout <<"Number "<<n++<<": "<< x << endl;
+//	}
+//
+//	return 0;
+//}
+//
---
+++ P3 - Eric_Ng_Boon_Lee/Q2.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//	
+//	
+//	char name[10];
+//
+//	cout << "Enter your name : ";
+//	cin.getline(name,10);
+//
+//	if (cin.fail())
+//		cout << "Name length exceeded" << endl;
+//	else
+//		cout << "Name entered :" << name << endl;
+//
+//	return 0;
+//}
