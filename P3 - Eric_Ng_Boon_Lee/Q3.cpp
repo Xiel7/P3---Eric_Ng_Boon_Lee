939ce309af79011310c4ef8e5ba7631a367e9dcc
Xiel7 <ericng402@gmail.com>
2020-07-04 22:22:30 +0800

    delete of comments

--- P3 - Eric_Ng_Boon_Lee/Q3.cpp
+++ P3 - Eric_Ng_Boon_Lee/Q3.cpp
@@ -1,45 +1,45 @@
-//#include <iostream>
-//
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int score[10], count = 0, total = 0, highCount = 0, lowCount = 0;
-//	double average;
-//
-//	cout << "Enter the scores (negative number to end the program)"<<endl;
-//
-//	for (int i = 0; i < (sizeof(score) / sizeof(*score)); i++)
-//	{
-//		cout << "Score " << ++count << ": ";
-//		cin >> score[i];
-//		if (score[i] < 0)
-//		{
-//			--count;
-//			break;
-//			
-//		}
-//		else 
-//			total += score[i];
-//
-//	}
-//
-//	average = (double)total / count;
-//
-//	for (int i = 0; i < count; i++)
-//	{
-//		if (score[i]>=average)
-//			highCount++;
-//		else
-//			lowCount++;
-//	}
-//	
-//	cout << "\nResults" << endl;
-//	cout << "=======\n" << endl;
-//	cout << "Average : " << average <<endl;
-//	cout << "Number of scores above or equal to the average is " << highCount << endl;
-//	cout << "Number of scores below the average is " << lowCount << endl;
-//	
-//	return 0;
-//}
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int score[10], count = 0, total = 0, highCount = 0, lowCount = 0;
+	double average;
+
+	cout << "Enter the scores (negative number to end the program)"<<endl;
+
+	for (int i = 0; i < (sizeof(score) / sizeof(*score)); i++)
+	{
+		cout << "Score " << ++count << ": ";
+		cin >> score[i];
+		if (score[i] < 0)
+		{
+			--count;
+			break;
+			
+		}
+		else 
+			total += score[i];
+
+	}
+
+	average = (double)total / count;
+
+	for (int i = 0; i < count; i++)
+	{
+		if (score[i]>=average)
+			highCount++;
+		else
+			lowCount++;
+	}
+	
+	cout << "\nResults" << endl;
+	cout << "=======\n" << endl;
+	cout << "Average : " << average <<endl;
+	cout << "Number of scores above or equal to the average is " << highCount << endl;
+	cout << "Number of scores below the average is " << lowCount << endl;
+	
+	return 0;
+}
